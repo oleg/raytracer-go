02a80a0611b65a5185af3290d1b15965e01aa0cb
Oleg Prozorov <pro.oleg@gmail.com>
2021-01-20 12:56:44 +0100

    add test for camera config

--- config/camera.go
+++ config/camera.go
@@ -1,12 +1,24 @@
 package config
 
 type Camera struct {
-	Size        CameraSize `yaml:"size"`
-	FieldOfView float64    `yaml:"field-of-view"`
-	//transform *geom.Matrix
+	Size  `yaml:"size"`
+	Sight `yaml:"sight"`
 }
 
-type CameraSize struct {
-	Horizontal int `yaml:"horizontal"`
-	Vertical   int `yaml:"vertical"`
+type Size struct {
+	Horizontal  int     `yaml:"horizontal"`
+	Vertical    int     `yaml:"vertical"`
+	FieldOfView float64 `yaml:"field-of-view"`
+}
+
+type Sight struct {
+	From Tuple `yaml:"from"`
+	To   Tuple `yaml:"to"`
+	Up   Tuple `yaml:"up"`
+}
+
+type Tuple struct {
+	X float64 `yaml:"x"`
+	Y float64 `yaml:"y"`
+	Z float64 `yaml:"z"`
 }
--- config/camera_test.go
+++ config/camera_test.go
@@ -1,25 +1,52 @@
 package config
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/yaml.v3"
 	"testing"
 )
 
 func Test_read_camera_config_from_string(t *testing.T) {
-	cameraConfig := `
-size: 
-    horizontal: 10
-    vertical: 5
-field-of-view: 3.312
+	cameraConfig := `size:
+  horizontal: 10
+  vertical: 5
+  field-of-view: 3.312
+sight:
+  from: { x: 1, y: 2, z: 3 }
+  to: { x: 30, y: 20, z: 10 }
+  up: { x: 5, y: 3, z: 4 } 
 `
+
 	var c Camera
 	err := yaml.Unmarshal([]byte(cameraConfig), &c)
+	fmt.Printf("%v\n", c)
 	assert.NoError(t, err)
+	assert.EqualValues(t,
+		Size{
+			Horizontal:  10,
+			Vertical:    5,
+			FieldOfView: 3.312,
+		},
+		c.Size)
 
-	if c.Size.Horizontal != 10 ||
-		c.Size.Vertical != 5 ||
-		c.FieldOfView != 3.312 {
-		t.Errorf("Wrong camera config %v", c)
-	}
+	assert.EqualValues(t,
+		Sight{
+			From: Tuple{
+				X: 1,
+				Y: 2,
+				Z: 3,
+			},
+			To: Tuple{
+				X: 30,
+				Y: 20,
+				Z: 10,
+			},
+			Up: Tuple{
+				X: 5,
+				Y: 3,
+				Z: 4,
+			},
+		},
+		c.Sight)
 }
